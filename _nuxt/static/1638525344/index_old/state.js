b85157950e49cefab4489704e049158d4c4dd8d3
danqmail@gmail.com <danqmail@gmail.com>
2021-12-03 05:01:15 -0500

    Updates

---
+++ _nuxt/static/1638525344/index_old/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdaqniel.github.io\u002F_nuxt\u002Fstatic\u002F1638525344",layout:"default",error:a,serverRendered:true,routePath:"\u002Findex_old",config:{_app:{basePath:"\u002Fdaqniel.github.io\u002F",assetsPath:"\u002Fdaqniel.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1638525344/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fszeliski","\u002Femptypage","\u002Findex_old","\u002Fexample","\u002Finspire"]})
